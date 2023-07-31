51391a2b5940a82e386ebffebfc5bd2700dedf04
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-07-31 01:23:05 -0300

    style: add a theme colors and global style

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Input } from './components/Input'
+import { Textarea } from './components/Textarea'
+
+function App() {
+  return (
+    <>
+      <Textarea placeholder={'digite aqui'} />
+      <Input Type="email" placeholder="E-mail" />
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/theme.js
@@ -0,0 +1,29 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#030711',
+    BACKGROUND_800: '#070C1A',
+    BACKGROUND_700: '#0F172A',
+
+    WHITE: '#F1F1F1',
+    GREE: '#00B37E',
+    PURPLE_500: '#7B61FF',
+    PURPLE_700: '#4f1ee3',
+
+    GRAY_200: '#c8cfd9',
+    GRAY_300: '#a2aebe',
+    GRAY_400: '#7f8ea3',
+    GRAY_500: '#596981',
+    GRAY_600: '#4d596d',
+    GRAY_700: '#434b5b',
+    GRAY_800: '#3c424e',
+    GRAY_900: '#20232c',
+    GRAY_950: '#20232c',
+
+    MIRAGE_700: '#314d73',
+    MIRAGE_800: '#2c4360',
+    MIRAGE_900: '#293951',
+    MIRAGE_950: '#1d283a',
+
+    RED: '#FF002E',
+  },
+}
